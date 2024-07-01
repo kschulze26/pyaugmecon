4bcbc39e7ac93f0c321c47bfdc3f418d0cb97f70
k.schulze <k.schulze@fz-juelich.de>
2024-07-01 10:17:41 +0200

    adjusted loop in for optimization specs to allow for 0 beeing passed - all changes within pyaugemcon are black related

--- pyaugmecon/options.py
+++ pyaugmecon/options.py
@@ -61,7 +61,7 @@ class Options:
 
         # Remove None values from dict when user has overriden them
         for key, value in dict(self.solver_opts).items():
-            if value is None or not value:
+            if value is None or (not value and value != 0):
                 del self.solver_opts[key]
 
         self.time_created = time.strftime(
